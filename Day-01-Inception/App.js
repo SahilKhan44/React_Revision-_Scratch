8d4bdf7cf270a7a497c3774279d8049daa3b8b21
Sahil khan <116485673+SahilKhan44@users.noreply.github.com>
2024-07-07 13:06:33 +0530

    Day o1

---
+++ Day-01-Inception/App.js
@@ -0,0 +1,22 @@
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const headingStyle ={
+//     color:"red",
+//     backgroundColor : "black"
+// }
+// const heading = React.createElement("h1", {id: "id",style: headingStyle} , "Hii This is hello");
+// root.render(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading = React.createElement("div", {}, [
+  React.createElement("div", { id: "parent1" }, [
+    React.createElement("h3", { id: "child3" }, "Hii This is child"),
+    React.createElement("h3", { id: "child4" }, "Hii This is child2"),
+  ]),
+  React.createElement("div", { id: "parent2" }, [
+    React.createElement("h3", { id: "child3" }, "Hii This is child"),
+    React.createElement("h3", { id: "child4" }, "Hii This is child2"),
+  ]),
+]);
+
+root.render(heading);
