1b9a2c8e0c693f1f7f833fc6a3978794e3ae138c
Sahil khan <116485673+SahilKhan44@users.noreply.github.com>
2024-07-10 21:09:51 +0530

    functional components

--- Day-01-Inception/App.js
+++ Day-01-Inception/App.js
@@ -44,6 +44,36 @@ import React from 'React';
 import ReactDOM from 'react-dom/client'
 
 
-const heading = React.createElement("h1", {id:"heading"},"Hello Sahil");
+// const heading = React.createElement("h1", {id:"heading"},"Hello Sahil");
+
+
+//Jsx
+// its transpiled before it reaches the JS engine -> parcel -> babel job to compile engine language
+const heading2 = <h1>Hii this is jsx</h1>
+
+// //React components
+// //Class based components old way
+// //functional way new 
+// When you write components then yopu have to start with capital letter 
+
+// Functional Components
+const Card = () => {
+  return (
+  <div>
+  <h1>This is other one</h1>
+  </div>
+)}
+
+const HeadingComponents = () => {
+return <div>
+  <Card/>
+  <h1>Sahil khan</h1>
+ 
+ </div> 
+}
+
+
+//when we render our functional components then we reander like that
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<HeadingComponents/>);
+// root.render(heading)
