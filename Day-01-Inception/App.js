e6c9e5971266189466f1dc196b6661226df4d6aa
Sahil khan <116485673+SahilKhan44@users.noreply.github.com>
2024-07-10 19:10:16 +0530

    revision to scratch app

--- Day-01-Inception/App.js
+++ Day-01-Inception/App.js
@@ -1,3 +1,35 @@
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+// // const headingStyle ={
+// //     color:"red",
+// //     backgroundColor : "black"
+// // }
+// // const heading = React.createElement("h1", {id: "id",style: headingStyle} , "Hii This is hello");
+// // root.render(heading);
+
+
+// import React from 'React';
+// import ReactDOM from 'react-dom/client'
+
+
+// // const heading = React.createElement("div", {}, [
+// //   React.createElement("div", { id: "parent1" }, [
+// //     React.createElement("h3", { id: "child3" }, "Hii This is child"),
+// //     React.createElement("h3", { id: "child4" }, "Hii This is child2"),
+// //   ]),
+// //   React.createElement("div", { id: "parent2" }, [
+// //     React.createElement("h3", { id: "child3" }, "Hii This is child"),
+// //     React.createElement("h3", { id: "child4" }, "Hii This is child2"),
+// //   ]),
+// // ]);
+
+// // console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+
+
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // const headingStyle ={
 //     color:"red",
@@ -5,18 +37,13 @@
 // }
 // const heading = React.createElement("h1", {id: "id",style: headingStyle} , "Hii This is hello");
 // root.render(heading);
+
+// scratch to new 
+
 import React from 'React';
+import ReactDOM from 'react-dom/client'
+
+
+const heading = React.createElement("h1", {id:"heading"},"Hello Sahil");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const heading = React.createElement("div", {}, [
-  React.createElement("div", { id: "parent1" }, [
-    React.createElement("h3", { id: "child3" }, "Hii This is child"),
-    React.createElement("h3", { id: "child4" }, "Hii This is child2"),
-  ]),
-  React.createElement("div", { id: "parent2" }, [
-    React.createElement("h3", { id: "child3" }, "Hii This is child"),
-    React.createElement("h3", { id: "child4" }, "Hii This is child2"),
-  ]),
-]);
-
-root.render(heading);
+root.render(heading)
